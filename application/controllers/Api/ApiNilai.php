930ee5a993d2e2742dcd07f62ff1ed299981576c
zenlie <jenly.ramdan3199@gmail.com>
2020-07-06 19:10:20 +0700

    modul nilai

---
+++ application/controllers/Api/ApiNilai.php
@@ -0,0 +1,114 @@
+<?php 
+
+header("Access-Controll-Allow-Origin: *");
+header("Content-Type: application/json, charset=UTF-8");
+
+Class ApiNilai extends CI_Controller {
+    function __construct()
+    {
+        parent::__construct();
+        $this->load->Model('ModelNilai');
+    }
+
+    public function index(){
+        $req = $_SERVER['REQUEST_METHOD'];
+        switch ($req) {
+            case 'GET':
+                $data = $this->ModelNilai->show()->result();  //show semua
+                if ($this->input->get('id_nilai') != '') {  // validate id_jadwal
+                    $id_nilai = $this->input->get('id_nilai'); // get id jdwl
+                    $data = $this->ModelNilai->show_one($id_nilai)->result_array(); //show one
+                }
+                echo json_encode($data); // if id_nilai kosong = shpw semua, if id_nilai tidak kosong = show one
+            break;
+            
+            case 'POST':
+                $data = $this->ModelNilai->add();
+ 
+                http_response_code(500);
+                $arrResult = array(
+                   'result' => false,
+                   'code' => 500,
+                   'message' => 'Internal Server Error'
+                );
+                
+                if ($data == 1) {
+                    http_response_code(201);
+                    $arrResult = array(
+                       'result' => true,
+                       'code' => 201,
+                       'message' => 'Data Was Created'
+                    );         
+           
+                 }
+                 echo json_encode($arrResult);
+
+            default:
+                # code...
+                break;
+            
+            case 'PUT':
+                    $putfp = fopen('php://input', 'r');
+                    $putdata = '';
+                    while($data = fread($putfp, 1024))
+                        $putdata .= $data;
+                    fclose($putfp);
+
+                    $var = json_decode($putdata);
+                    foreach ($var as $key => $value) {
+                        $$key = $value;
+                    }
+                    http_response_code(404);
+                    $arrResult = array(
+                        'result' => false,
+                        'code' => 404,
+                        'message' => 'Data Not Found'
+                    );
+                    $result = $this->ModelNilai->show_one($id_nilai)->result();
+                    $data = $this->ModelNilai->update($id_nilai, $nilai, $nim_mhs, $kd_matkul);
+                    if (count($result) == 1) {
+                        http_response_code(200);
+                        $arrResult = array(
+                            'result' => true,
+                            'code' => 200,
+                            'message' => 'Data Was Updated'
+                        );
+                    }
+                    echo json_encode($arrResult);
+                break;
+
+                case 'DELETE':
+                    $putfp = fopen('php://input', 'r');
+                    $putdata = '';
+                    while($data = fread($putfp, 1024))
+                       $putdata .= $data;
+                    fclose($putfp);
+                    $var = json_decode($putdata);
+                    foreach ($var as $key => $value) {
+                       $$key = $value;
+                    }
+     
+                    http_response_code(404);
+                    $arrResult = array(
+                       'result' => false,
+                       'code' => 404,
+                       'message' => 'Data Not Found'
+                    );
+                    $result = $this->ModelNilai->show_one($id_nilai)->result();         
+                    if (count($result) == 1) {
+                       $data = $this->ModelNilai->delete($id_nilai);
+                       http_response_code(202);
+                       $arrResult = array(
+                          'result' => true,
+                          'code' => 202,
+                          'message' => 'Data Was Deleted'
+                       );
+     
+                    }
+                    echo json_encode($arrResult);
+     
+                  break;
+        }
+    }
+
+}
---
+++ application/models/ModelNilai.php
@@ -0,0 +1,64 @@
+<?php
+Class ModelNilai extends CI_Model{
+
+    public function show()
+    {
+        $this->db->select('*'); //take1
+        $this->db->from('nilai'); //take1
+        $this->db->join('mahasiswa', 'nilai.nim_mhs = mahasiswa.nim_mhs'); //take1
+        $this->db->join('kd_matkul', 'nilai.kd_matkul = kd_matkul.kd_matkul'); //take1
+        $data = $this->db->get(); //take1
+        return $data; //take1
+    }
+
+    public function show_one($id)
+    {
+        $this->db->select('*'); //take2
+        $this->db->from('nilai'); //take2
+        $this->db->join('mahasiswa', 'nilai.nim_mhs = mahasiswa.nim_mhs'); //take2
+        $this->db->join('kd_matkul', 'nilai.kd_matkul = kd_matkul.kd_matkul'); //take2
+        $this->db->where('id_nilai', $id); //take2
+        $data = $this->db->get(); //take2
+        return $data; //take2
+    }  
+
+    public function add()
+    {
+        $data = [
+            'id_nilai' => $this->input->post('id_nilai'),
+            'nilai' => $this->input->post('nilai'),
+            'nim_mhs' => $this->input->post('nim_mhs'),
+            'kd_matkul' => $this->input->post('kd_matkul'),
+        ];
+        return $this->db->insert('nilai',$data);
+    }
+
+    public function edit($id)
+    {
+        $data=$this->db->get_where('nilai',array('id_nilai'=>$id));
+        return $data;
+    }
+
+    public function update($id_nilai, $nilai, $nim_mhs, $kd_matkul)
+    {
+        $data=[
+            'nilai' => $nilai,
+            'nim_mhs' => $nim_mhs,
+            'kd_matkul' => $kd_matkul,
+         ];
+         $this->db->where('id_nilai',$id_nilai);
+         return $this->db->update('nilai',$data);
+
+    }
+
+    public function delete($id_nilai)
+    {
+
+        $id_nilai = $id_nilai;
+        $this->db->where('id_nilai',$id_nilai);
+        return $this->db->delete('nilai');
+
+    }    
+
+}
+?>
