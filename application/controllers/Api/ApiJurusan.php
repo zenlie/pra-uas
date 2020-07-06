ff1fded060f2095fab3ac7e46754fea7ce95ed6e
zenlie <jenly.ramdan3199@gmail.com>
2020-07-06 17:33:35 +0700

    modul jurusan

---
+++ application/controllers/Api/ApiJurusan.php
@@ -0,0 +1,114 @@
+<?php 
+
+header("Access-Controll-Allow-Origin: *");
+header("Content-Type: application/json, charset=UTF-8");
+
+Class ApiJurusan extends CI_Controller {
+    function __construct()
+    {
+        parent::__construct();
+        $this->load->Model('ModelJurusan');
+    }
+
+    public function index(){
+        $req = $_SERVER['REQUEST_METHOD'];
+        switch ($req) {
+            case 'GET':
+                $data = $this->ModelJurusan->show()->result();  //take1
+                if ($this->input->get('kd_jurusan') != '') {  //take 1 take2
+                    $kd_jurusan = $this->input->get('kd_jurusan'); //take 1 take2
+                    $data = $this->ModelJurusan->show_one($kd_jurusan)->result_array(); //take2
+                } //take2
+                echo json_encode($data); //take2
+            break;
+            
+            case 'POST':
+                $data = $this->ModelJurusan->add();
+ 
+                http_response_code(500);
+                $arrResult = array(
+                   'result' => false,
+                   'code' => 500,
+                   'message' => 'Internal Server Error'
+                );
+                
+                if ($data == 1) {
+                    http_response_code(201);
+                    $arrResult = array(
+                       'result' => true,
+                       'code' => 201,
+                       'message' => 'Data Was Created'
+                    );         
+           
+                 }
+                 echo json_encode($arrResult);
+
+            default:
+                # code...
+                break;
+            
+            case 'PUT':
+                    $putfp = fopen('php://input', 'r');
+                    $putdata = '';
+                    while($data = fread($putfp, 1024))
+                        $putdata .= $data;
+                    fclose($putfp);
+
+                    $var = json_decode($putdata);
+                    foreach ($var as $key => $value) {
+                        $$key = $value;
+                    }
+                    http_response_code(404);
+                    $arrResult = array(
+                        'result' => false,
+                        'code' => 404,
+                        'message' => 'Data Not Found'
+                    );
+                    $result = $this->ModelJurusan->show_one($kd_jurusan)->result();
+                    $data = $this->ModelJurusan->update($kd_jurusan, $nm_jurusan, $kd_jenjang);
+                    if (count($result) == 1) {
+                        http_response_code(200);
+                        $arrResult = array(
+                            'result' => true,
+                            'code' => 200,
+                            'message' => 'Data Was Updated'
+                        );
+                    }
+                    echo json_encode($arrResult);
+                break;
+
+                case 'DELETE':
+                    $putfp = fopen('php://input', 'r');
+                    $putdata = '';
+                    while($data = fread($putfp, 1024))
+                       $putdata .= $data;
+                    fclose($putfp);
+                    $var = json_decode($putdata);
+                    foreach ($var as $key => $value) {
+                       $$key = $value;
+                    }
+     
+                    http_response_code(404);
+                    $arrResult = array(
+                       'result' => false,
+                       'code' => 404,
+                       'message' => 'Data Not Found'
+                    );
+                    $result = $this->ModelJurusan->show_one($kd_jurusan)->result();         
+                    if (count($result) == 1) {
+                       $data = $this->ModelJurusan->delete($kd_jurusan);
+                       http_response_code(202);
+                       $arrResult = array(
+                          'result' => true,
+                          'code' => 202,
+                          'message' => 'Data Was Deleted'
+                       );
+     
+                    }
+                    echo json_encode($arrResult);
+     
+                  break;
+        }
+    }
+
+}
---
+++ application/models/ModelJurusan.php
@@ -0,0 +1,60 @@
+<?php
+Class ModelJurusan extends CI_Model{
+
+    public function show()
+    {
+        $this->db->select('*'); //take1
+        $this->db->from('jurusan'); //take1
+        $this->db->join('jenjang', 'jurusan.kd_jenjang = jenjang.kd_jenjang'); //take1
+        $data = $this->db->get(); //take1
+        return $data; //take1
+    }
+
+    public function show_one($id)
+    {
+        $this->db->select('*'); //take2
+        $this->db->from('jurusan'); //take2
+        $this->db->join('jenjang', 'jurusan.kd_jenjang = jenjang.kd_jenjang'); //take2
+        $this->db->where('kd_jurusan', $id); //take2
+        $data = $this->db->get(); //take2
+        return $data; //take2
+    }  
+
+    public function add()
+    {
+        $data = [
+            'kd_jurusan ' => $this->input->post('kd_jurusan'),
+            'nm_jurusan' => $this->input->post('nm_jurusan'),
+            'kd_jenjang' => $this->input->post('kd_jenjang'),
+        ];
+        return $this->db->insert('jurusan',$data);
+    }
+
+    public function edit($id)
+    {
+        $data=$this->db->get_where('jurusan',array('kd_jurusan'=>$id));
+        return $data;
+    }
+
+    public function update($kd_jurusan, $nm_jurusan, $kd_jenjang)
+    {
+        $data=[
+            'nm_jurusan' => $nm_jurusan,
+            'kd_jenjang' -> $kd_jenjang,
+         ];
+         $this->db->where('kd_jurusan',$kd_jurusan);
+         return $this->db->update('jurusan',$data);
+
+    }
+
+    public function delete($kd_jurusan)
+    {
+
+        $kd_jurusan = $kd_jurusan;
+        $this->db->where('kd_jurusan',$kd_jurusan);
+        return $this->db->delete('jurusan');
+
+    }    
+
+}
+?>
