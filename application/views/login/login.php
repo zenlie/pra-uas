353100633cf296dee3658aac4166c280204de05e
zenlie <jenly.ramdan3199@gmail.com>
2020-07-08 15:32:20 +0700

    fix title

--- application/views/login/login.php
+++ application/views/login/login.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title>AdminLTE 3 | Log in</title>
+  <title>PRA | UAS</title>
   <!-- Tell the browser to be responsive to screen width -->
   <meta name="viewport" content="width=device-width, initial-scale=1">
 
@@ -21,7 +21,7 @@
 <body class="hold-transition login-page">
 <div class="login-box">
   <div class="login-logo">
-    <a href="../../index2.html"><b>Admin</b>LTE</a>
+    <a href="../../index2.html"><b>PRA</b>UAS</a>
   </div>
   <!-- /.login-logo -->
   <div class="card">
